ab0eaa561d70461997bf00fa717e318b9f01a092
octocatz <lanzartista@gmail.com>
2020-09-11 01:58:39 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-//mockup
+//mockup sample
 void main() => runApp(MyApp());
 
 final dummySnapshot = [
